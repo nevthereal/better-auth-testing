0df26bd1148af2fb222c01186d3920f48dcddba1
Neville Brem <bremneville@gmail.com>
2024-10-02 06:50:18 +0200

    added gh

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,19 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from './db';
+import { GH_CLIENT_ID, GH_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'sqlite'
-	})
+	}),
+	emailAndPassword: {
+		enabled: false
+	},
+	socialProviders: {
+		github: {
+			clientId: GH_CLIENT_ID,
+			clientSecret: GH_CLIENT_SECRET
+		}
+	}
 });
